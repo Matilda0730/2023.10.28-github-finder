4e100568264741d7cba2de20bc2d4b6fc1c66224
Matilda007 <matilda230730@gmail.com>
2023-10-28 16:31:54 +0900

    OOP를 이용하는 코드를 넣었습니다.

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+class GithubPerson {
+  name;
+  publicRepos;
+  publicGists;
+  followers;
+  following;
+  company;
+  website;
+  location;
+  memberSince;
+
+  constructor(
+    name,
+    publicRepos,
+    publicGists,
+    followers,
+    following,
+    company,
+    website,
+    location,
+    memberSince
+  ) {
+    this.name = name;
+    this.publicRepos = publicRepos;
+    this.publicGists = publicGists;
+    this.followers = followers;
+    this.following = following;
+    this.company = company;
+    this.website = website;
+    this.location = location;
+    this.memberSince = memberSince;
+  }
+}
+
+const githubName = new GithubPerson();
+console.log(githubName.name);
+
 const githubForm = document.querySelector("#github-form");
 const githubUsernameInput = document.querySelector("#github-username");
 
